f49c9de3b7254243941a4a14d2fee4d88f8804cb
rizkysbgj <rizkysubagja123@gmail.com>
2017-05-29 08:22:39 +0700

    Buat Jonggi

--- item.sql
+++ item.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.4.14
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2017 at 04:39 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: May 29, 2017 at 03:21 AM
+-- Server version: 5.6.26
+-- PHP Version: 5.5.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `item`
 --
 
-CREATE TABLE `item` (
+CREATE TABLE IF NOT EXISTS `item` (
   `item_id` int(11) NOT NULL,
   `nama_item` varchar(255) NOT NULL,
   `deskripsi_item` text NOT NULL,
@@ -36,8 +36,19 @@ CREATE TABLE `item` (
   `diskon` int(11) NOT NULL,
   `penjualan` int(11) NOT NULL,
   `fresh` int(11) NOT NULL,
-  `pengunjung` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `pengunjung` int(11) NOT NULL,
+  `gambar_item` varchar(1000) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `kategori` varchar(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`item_id`, `nama_item`, `deskripsi_item`, `quantity`, `harga`, `berat`, `diskon`, `penjualan`, `fresh`, `pengunjung`, `gambar_item`, `id_user`, `kategori`) VALUES
+(24, 'Pancingan Wokeh', 'Mata Pancing... YESSS!!!!', 1, 99999, 99999, 0, 0, 0, 2, '149595781110.jpg', 10, 'aksesoris'),
+(25, 'Ikan Gurame', 'Ikan Gurame sehat', 1, 1000, 10, 0, 0, 0, 0, '149602020510.jpg', 10, 'konsumsi');
 
 --
 -- Indexes for dumped tables
@@ -47,7 +58,8 @@ CREATE TABLE `item` (
 -- Indexes for table `item`
 --
 ALTER TABLE `item`
-  ADD PRIMARY KEY (`item_id`);
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `item_id` (`item_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -57,7 +69,7 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- user.sql
+++ user.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.4.14
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2017 at 11:04 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: May 29, 2017 at 03:22 AM
+-- Server version: 5.6.26
+-- PHP Version: 5.5.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,22 +26,26 @@ SET time_zone = "+00:00";
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id_user` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `alamat` text NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `jk` varchar(1) NOT NULL,
-  `notelp` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `notelp` varchar(20) NOT NULL,
+  `jenis` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `nama`, `alamat`, `email`, `password`, `jk`, `notelp`) VALUES
-(8, 'Ariel Malada', 'Jl.Kayu Putih Utara 3E', 'vauxhallvectravxr@gmail.com', '39e9d130e6936441d85e8b7e28860823', 'L', '+6287883742434');
+INSERT INTO `user` (`id_user`, `nama`, `alamat`, `email`, `password`, `jk`, `notelp`, `jenis`) VALUES
+(8, 'Ariel Malada', 'Jl.Kayu Putih Utara 3E', 'vauxhallvectravxr@gmail.com', '39e9d130e6936441d85e8b7e28860823', 'L', '+6287883742434', 1),
+(9, 'Rizky', 'Bekasi', 'rizkysubagja99@gmail.com', 'b36d331451a61eb2d76860e00c347396', 'L', '085691958469', 0),
+(10, 'Tukang Ikan', 'Pasar Ikan', 'tukangikan@gmail.com', 'b36d331451a61eb2d76860e00c347396', 'L', '14045', 1),
+(11, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', '', '', 0);
 
 --
 -- Indexes for dumped tables
@@ -61,7 +65,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
